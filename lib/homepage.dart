54ced401a2de05496e6801e74d0fa326bb8bb949
zukermsalis <zukermsalis@gmail.com>
2024-09-12 08:27:16 +0000

    atividade TelaTinder

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,9 +1,9 @@
-import 'package:flutter/cupertino.dart';
+
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 
 class Homepage extends StatelessWidget {
   const Homepage({super.key});
+
 
   @override
   Widget build(BuildContext context) {
@@ -21,79 +21,85 @@ class Homepage extends StatelessWidget {
               ),
             ),
           ),
-
-          // Os widgets ficam sobre o gradiente
           Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              const SizedBox(
-                height: 20,
-              ),
+              const SizedBox(height: 20),
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   Row(
+                    //cabelo ta é caindo
+
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+
                     children: [
                       SizedBox(
-                        width: 70,
-                        height: 70,
+                        width: 45,
+                        height: 45,
                         child: Image.network(
-                            'https://img.icons8.com/?size=48&id=2U1pv20TnZVf&format=png'),
-                      ),
-                      const Center(
-                        child: Text(
-                          'Tinder',
-                          style: TextStyle(
-                              fontSize: 60,
-                              fontFamily: 'Pacific',
-                              color: Colors.white,
-                              fontWeight: FontWeight.bold),
+                          'https://img.icons8.com/?size=48&id=2U1pv20TnZVf&format=png',
                         ),
                       ),
+                   
+                      Text(
+                        'tinder',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 42,
+                          fontWeight: FontWeight.bold,
+                        
+                        ),
+                      )
                     ],
-                  )
+                  ),
                 ],
               ),
-              const SizedBox(
-                height: 10,
-              ),
+              const SizedBox(height: 190),
               Column(
                 children: [
                   const Center(
                     child: Text(
                       'By tapping Create Account or Sign In, you agree to our',
                       style: TextStyle(
-                          color: Colors.white,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 15),
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 15,
+                      ),
                     ),
                   ),
                   const Center(
-                    child: Text.rich(
-                      TextSpan(
-                        text: ' Terms ',
-                        style: TextStyle(
-                          color: Colors.white,
-                          decoration: TextDecoration.underline,
-                          decorationColor: Colors.white,
-                          fontWeight: FontWeight.bold,
-                        ),
-                        children: [
-                          TextSpan(
-                            text: ' Learn how we Process Your Data, in Your  ',
-                            style: TextStyle(
+                    child: InkWell(
+                      child: Text.rich(
+                        TextSpan(
+                          text: ' Terms ',
+                          style: TextStyle(
+                            color: Colors.white,
+                            decoration: TextDecoration.underline,
+                            decorationColor: Colors.white,
+                            fontWeight: FontWeight.bold,
+                          ),
+                          children: [
+                            TextSpan(
+                              text:
+                                  ' Learn how we Process Your Data, in Your  ',
+                              style: TextStyle(
                                 decoration: TextDecoration.none,
                                 color: Colors.white,
-                                fontWeight: FontWeight.bold),
-                          ),
-                          TextSpan(
-                            text: 'Privacy',
-                            style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            TextSpan(
+                              text: 'Privacy',
+                              style: TextStyle(
                                 decoration: TextDecoration.underline,
                                 decorationColor: Colors.white,
-                                fontWeight: FontWeight.bold),
-                          ),
-                        ],
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ],
+                        ),
                       ),
                     ),
                   ),
@@ -102,108 +108,122 @@ class Homepage extends StatelessWidget {
                       TextSpan(
                         text: ' Policy',
                         style: TextStyle(
-                            decoration: TextDecoration.underline,
-                            decorationColor: Colors.white,
-                            color: Colors.white,
-                            fontWeight: FontWeight.bold),
+                          decoration: TextDecoration.underline,
+                          decorationColor: Colors.white,
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                        ),
                         children: [
                           TextSpan(
                             text: ' and ',
                             style: TextStyle(
-                                decoration: TextDecoration.none,
-                                fontWeight: FontWeight.bold),
+                              decoration: TextDecoration.none,
+                              fontWeight: FontWeight.bold,
+                            ),
                           ),
                           TextSpan(
                             text: 'Cookies Policy',
                             style: TextStyle(
-                                decoration: TextDecoration.underline,
-                                decorationColor: Colors.white,
-                                fontWeight: FontWeight.bold),
+                              decoration: TextDecoration.underline,
+                              decorationColor: Colors.white,
+                              fontWeight: FontWeight.bold,
+                            ),
                           ),
                         ],
                       ),
                     ),
                   ),
+                  const SizedBox(height: 60),
                   Column(
                     children: [
-                      const SizedBox(
-                        height: 30,
+                      const SizedBox(height: 30),
+                      Container(
+                        width: 370,
+                        height: 90,
+                        decoration: BoxDecoration(
+                          gradient: const LinearGradient(
+                            colors: [Color(0XFFE4645F), Color(0XFFE45174)],
+                          ),
+                          borderRadius: BorderRadius.circular(40),
+                          border: Border.all(
+                            color: Colors.white,
+                            width: 4,
+                          ),
+                        ),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            const SizedBox(
+                              width: 20,
+                              height: 70,
+                              child: Icon(
+                                Icons.apple,
+                                color: Colors.white,
+                                size: 60,
+                              ),
+                            ),
+                            const SizedBox(width: 70),
+                            const Center(
+                              child: Text(
+                                'SIGN IN WITH APPLE',
+                                style: TextStyle(
+                                  fontSize: 18,
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
                       ),
-                      Container(
-                          width: 370,
-                          height: 90,
-                          decoration: BoxDecoration(
-                            gradient: LinearGradient(
-                                colors: [Color(0XFFE4645F), Color(0XFFE45174)]),
-                            borderRadius: BorderRadius.circular(40),
-                            border: Border.all(
-                              color: Colors.white,
-                              width: 4,
-                            ),
-                          ),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.start,
-                            children: [
-                              const SizedBox(
-                                width: 30,
-                                height: 70,
-                                child: Icon(Icons.apple,
-                                    color: Colors.white, size: 60),
-                              ),
-                              const SizedBox(
-                                width: 50,
-                              ),
-                              const Center(
-                                child: Text(
-                                  'SIGN IN WITH APPLE',
-                                  style: TextStyle(
-                                      fontSize: 18,
-                                      color: Colors.white,
-                                      fontWeight: FontWeight.bold),
-                                ),
-                              ),
-                            ],
-                          )),
-                      const SizedBox(height: 10),
-                      Container(
-                          height: 90,
-                          width: 370,
-                          decoration: BoxDecoration(
-                            gradient: LinearGradient(
-                                colors: [Color(0XFFE4645F), Color(0XFFE45174)]),
-                            borderRadius: BorderRadius.circular(40),
-                            border: Border.all(
-                              color: Colors.white,
-                              width: 4,
-                            ),
-                          ),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.start,
-                            children: [
-                              const SizedBox(
-                                width: 80,
-                                height: 60,
-                                child: Icon(Icons.facebook,
-                                    color: Colors.white, size: 60),
-                              ),
-                              const Center(
-                                child: Text(
-                                  'SIGN IN WITH FACEBOOK',
-                                  style: TextStyle(
-                                      fontSize: 18,
-                                      color: Colors.white,
-                                      fontWeight: FontWeight.bold),
-                                ),
-                              )
-                            ],
-                          )),
                       const SizedBox(height: 10),
                       Container(
                         height: 90,
                         width: 370,
                         decoration: BoxDecoration(
-                          gradient: LinearGradient(
-                              colors: [Color(0XFFE4645F), Color(0XFFE45174)]),
+                          gradient: const LinearGradient(
+                            colors: [Color(0XFFE4645F), Color(0XFFE45174)],
+                          ),
+                          borderRadius: BorderRadius.circular(40),
+                          border: Border.all(
+                            color: Colors.white,
+                            width: 4,
+                          ),
+                        ),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            const SizedBox(
+                              width: 80,
+                              height: 60,
+                              child: Icon(
+                                Icons.facebook,
+                                color: Colors.white,
+                                size: 60,
+                              ),
+                            ),
+                         
+                            const Center(
+                              child: Text(
+                                'SIGN IN WITH FACEBOOK',
+                                style: TextStyle(
+                                  fontSize: 18,
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      const SizedBox(height: 10),
+                      Container(
+                        height: 90,
+                        width: 370,
+                        decoration: BoxDecoration(
+                          gradient: const LinearGradient(
+                            colors: [Color(0XFFE4645F), Color(0XFFE45174)],
+                          ),
                           borderRadius: BorderRadius.circular(40),
                           border: Border.all(
                             color: Colors.white,
@@ -221,27 +241,35 @@ class Homepage extends StatelessWidget {
                                 size: 60,
                               ),
                             ),
+                           
                             Center(
                               child: Text(
                                 'SIGN IN WITH PHONE NUMBER',
                                 style: TextStyle(
-                                    fontSize: 18,
-                                    color: Colors.white,
-                                    fontWeight: FontWeight.bold),
+                                  fontSize: 18,
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold,
+                                ),
                               ),
-                            )
+                            ),
                           ],
                         ),
                       ),
-// Suggested code may be subject to a license. Learn more: ~LicenseLog:2238822559.
-                    SizedBox(height: 5,),  Center(
-                          child: Text(
-                        'Trouble Signing in?',
-                        style: TextStyle(
+                      
+                     SizedBox(height: 20,), const Center(
+                        child: Text(
+                          'Trouble Signing in?',
+                          style: TextStyle(
                             color: Colors.white,
                             fontWeight: FontWeight.bold,
-                            fontSize: 20),
-                      )),
+                            fontSize: 20,
+                            
+                          
+                          ),
+                          
+                        ),
+                        
+                      ),
                     ],
                   ),
                 ],
